9ed23b3937f3c375caff2d428da940c129fc1294
SofiiaKozlyk <ipz222_kso@student.ztu.edu.ua>
2024-04-27 18:14:23 +0300

    Added Iterator in Lab05

--- Lab05/CompositeClassLibrary/LightNode.cs
+++ Lab05/CompositeClassLibrary/LightNode.cs
@@ -1,16 +1,27 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace CompositeClassLibrary
 {
-    public abstract class LightNode
+    public abstract class LightNode : IEnumerable<LightNode>
     {
         public abstract string OuterHTML { get; }
 
         public abstract string InnerHTML { get; }
         public abstract string Display(int depth);
+        public virtual IEnumerator<LightNode> GetEnumerator()
+        {
+            yield return this;
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
     }
 }
